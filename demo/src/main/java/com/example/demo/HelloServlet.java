414f51de68e9f3ca4521691381af9af68c59860a
xyc1719 <38127251+xyc1719@users.noreply.github.com>
2023-10-16 16:57:58 +0800

    current bench

---
+++ demo/src/main/java/com/example/demo/HelloServlet.java
@@ -0,0 +1,33 @@
+package com.example.demo;
+
+import java.io.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+@WebServlet(name = "helloServlet", value = "/hello-servlet")
+public class HelloServlet extends HttpServlet {
+    private String message;
+    private String anotherMessage;
+
+    public void init() {
+        message = "Hello World!";
+        anotherMessage="中文支持";
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html;charset=gb2312");
+        request.setCharacterEncoding("gb2312");//以上两句用于支持中文
+        // Hello
+        PrintWriter out = response.getWriter();
+        out.println("<html>");
+        out.println("<head><title>Page 2</title></head>");
+        out.println("<body>");
+        //out.println("<html><body>");
+        out.println("<h1>" + message + "</h1>");
+        out.println("<h3>"+ anotherMessage+"</h3>");
+        out.println("</body></html>");
+    }
+
+    public void destroy() {
+    }
+}
---
+++ demo/src/main/webapp/index.jsp
@@ -0,0 +1,16 @@
+<%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>this is the title.</title>
+</head>
+<body>
+<!--print by jsp-->
+<h1><%= "Hello World! ---by JSP" %></h1>
+<br/>
+<!--print by html-->
+<h2>Hi the World!  ---by html</h2>
+<br/>
+<a href="hello-servlet">Visit Hello Servlet</a>
+</body>
+</html>
